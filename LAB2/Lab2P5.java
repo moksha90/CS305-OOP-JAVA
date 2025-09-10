5ff5ee799aa69ba0a48a9c5943d5dd11a58f07d5
moksha90 <mokshagubba18@gmail.com>
2025-09-10 12:34:36 +0530

    Add files via upload

---
+++ LAB2/Lab2P5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Lab2P5{
+public static void main(String...args){
+			Scanner s = new Scanner(System.in);
+			int a =s.nextInt();
+			int count = 0;
+			
+			int i = 2;
+			while(i<a){
+			
+		}
+		System.out.println("Result is: "+str);
+
+	}
+}
+
