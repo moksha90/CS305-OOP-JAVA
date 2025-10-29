20ee1cbaf959c657fd310126a865ea9f387fbb43
moksha90 <mokshagubba18@gmail.com>
2025-10-29 10:12:23 +0530

    Add files via upload

---
+++ LAB6/Lab6P1.java
@@ -0,0 +1,11 @@
+enum fruits{
+		Apple,Banana,Citrus,Dragonfruit,Fig,Grapes;
+}
+public class Lab6P1 {
+		public static void main(String...args){
+		for(fruits f: fruits.values()){
+		System.out.println(f);
+		}
+		}
+	}	
+
